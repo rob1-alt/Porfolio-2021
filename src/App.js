32117096b12239863219c77da730076795b09169
Rob1-alt <robin.pautigny@hetic.net>
2021-12-14 23:19:43 +0100

    adding main page

--- src/App.js
+++ src/App.js
@@ -9,9 +9,20 @@ function App() {
       <header className="App-header">
         
               <div className='box'>
+              <p className='mainText'>
+                  Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à <span className='vert'>Hetic</span>.
+                  Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.
+                </p>
               </div>
           
               <div className='box2'>
+                <nav className='wrapperMenu'>
+                  <ol>
+                    <li>À propos de moi</li>
+                    <li>Projets</li>
+                    <li>Contact</li>
+                  </ol>
+                </nav>
               </div>
               
       </header>
