b6ffbd0a54984d07750ce8fec45f83eda1b535ed
Rob1 <robin.pautigny@hetic.net>
2021-12-15 11:04:39 +0100

    favicon add, document name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Row, Col, Divider} from 'antd';
 import 'antd/dist/antd.css';
+import { Helmet } from 'react-helmet'
+
+const title = 'Robin Pautigny'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+          <title>{ title }</title>
+        </Helmet>
       <header className="App-header">
-        
+        <div className='wrapperHome'>
+          
               <div className='box'>
               <p className='mainText'>
                   Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à <span className='vert'>Hetic</span>.
                   Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.
                 </p>
               </div>
-          
               <div className='box2'>
                 <nav className='wrapperMenu'>
                   <ol>
@@ -24,6 +29,8 @@ function App() {
                   </ol>
                 </nav>
               </div>
+        </div>
+        
               
       </header>
     </div>
