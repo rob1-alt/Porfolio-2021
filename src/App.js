40511ecb14dfa2a912628ae831b01a432b5ba57d
Rob1 <robin.pautigny@hetic.net>
2021-12-30 23:20:05 +0100

    nothing special

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import { useState, useEffect } from "react";
 import './App.css';
 import './index.js'
 import 'antd/dist/antd.css';
-// eslint-disable-next-line
-import ellipse from './img/Ellipse1.png';
-// eslint-disable-next-line
-import ellipseDown from './img/Ellipse2.png';
-// eslint-disable-next-line
-import rectangle from './img/Rectangle6.png';
 import { Helmet } from 'react-helmet'
+import Hamburger from 'react-hamburgers';
 
 
 
@@ -24,7 +19,7 @@ function App() {
           <title>{ title }</title>
         </Helmet>
       <header className="App-header">
-        <p className=" description text-left p-16 mr-64">Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à <span className="hover:underline heticSchool">Hetic</span>.
+        <p className=" description">Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à <span className="hover:underline heticSchool">Hetic</span>.
           Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.</p>
       </header>
     </div>
