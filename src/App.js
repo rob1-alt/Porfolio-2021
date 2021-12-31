f7da709272aac748bd2b1cf8ac60debee445010c
Rob1 <robin.pautigny@hetic.net>
2021-12-31 02:28:19 +0100

    halburger menu

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 import './index.js'
 import 'antd/dist/antd.css';
 import { Helmet } from 'react-helmet'
+import { reveal as Menu } from 'react-burger-menu'
+
+
 
 
 
 const title = 'Robin Pautigny 🛠'
+
+
 
 
 
@@ -18,6 +23,11 @@ function App() {
           <title>{ title }</title>
         </Helmet>
       <header className="App-header">
+      <Menu>
+        <a id="home" className="menu-item" href="/">À propos de moi</a>
+        <a id="about" className="menu-item" href="/about">Projets</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+      </Menu>
         <p className=" description">Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à <span className="hover:underline heticSchool">Hetic</span>.
           Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.</p>
       </header>
